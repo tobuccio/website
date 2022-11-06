ff2f65da4dc20dfbd7c43e4b71de65b3e8fc7fcd
tobuccio <ladiesman.bobi@gmail.com>
2022-11-05 21:35:05 -0400

    Weather api implementation

--- main/webspace/src/App.js
+++ main/webspace/src/App.js
@@ -1,23 +1,62 @@
+import React from 'react'
+import { useState, useEffect } from 'react';
 import './App.css';
+//import images
+//START WEATHER API
 
-const Person = () => {
-  return (
-    <>
-      <h1> name : fart</h1>
-      <h2> last : ball </h2>
-      <h2> age : 30 </h2>
-    </>
-  )
+const API_KEY_W = '5818deb42cf8a589318779074872fec4';
+const lat = 41.99
+const lon = -72.54
+const API_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + API_KEY_W;
+
+//KEY: 5818deb42cf8a589318779074872fec4
+//https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+// 60 calls per min of curr weather
+// codes: (ID PARAM) https://openweathermap.org/weather-conditions
+
+const Weather = () => {
+
+    const [weatherdata, setWeatherData] = useState([]);
+    const updateWeather = async () => {
+        const response = await fetch(`${API_WEATHER_URL}`);
+        const data = await response.json();
+        setWeatherData(data.weather[0].main);
+    }
+    useEffect (()=> {
+        updateWeather();
+    }, []);
+
+    return (
+        // PUT DATA HERE
+        <div className = 'Weather'>
+            <h2> Current weather {weatherdata}</h2>
+        </div>
+    );
 }
+
+
 const App = () => {
-  const name = "test";
-  const isName = false;
-  return (
-    <div className="App">
-    <Person />
-    <h1> Main Page Three Buttons </h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1> Welcome to FOMO </h1>
+
+            <div className = "WeatherDisplay">
+                <Weather />
+            </div>
+
+            <div className = "Buttons">
+                <div className='ProjectButton'>
+
+                </div>
+                <div className='AboutButton'>
+
+                </div>
+                <div className='GameButton'>
+
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
